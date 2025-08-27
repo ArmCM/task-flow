6926ca7aa89aed67b9043550973fc8cd7d4b81a2
λrmando <ackurtcobain15@gmail.com>
2025-08-26 23:31:54 -0600

    Improves API responses and error handling

--- core/Response.php
+++ core/Response.php
@@ -6,9 +6,11 @@ class Response {
     const int HTTP_OK = 200;
     const int HTTP_CREATED = 201;
     const int HTTP_NO_CONTENT = 204;
+    const int BAD_REQUEST = 400;
     const int HTTP_UNAUTHORIZED = 401;
     const int FORBIDDEN = 403;
     const int NOT_FOUND = 404;
+    const int UNPROCESSABLE_CONTENT  = 422;
     const int INTERNAL_SERVER_ERROR = 500;
 
     public static function json(array $data, int $status = 200)
